10b68ad3a1ff2fac32cc7bc7a09d976dd51f95d0
O. R. Toimela <ort@hiisi.digital>
2026-06-19 04:53:25 +0300

    chore: remove unused impl_trait_in_assoc_type gate from hilavitkutin-api
    * chore: open api unused-feature-gate topic
    
    * docs: open doc CL for api unused-feature-gate
    
    * chore: open src CL for api unused-feature-gate
    
    * chore: remove unused impl_trait_in_assoc_type gate from hilavitkutin-api
    
    * chore: lock src CL for api unused-feature-gate
    
    * chore: close round 202606192100 api feature-gate

--- mock/crates/hilavitkutin-api/src/lib.rs
+++ mock/crates/hilavitkutin-api/src/lib.rs
@@ -13,7 +13,6 @@
 #![feature(adt_const_params)]
 #![feature(const_ops)]
 #![feature(const_trait_impl)]
-#![feature(impl_trait_in_assoc_type)]
 #![feature(marker_trait_attr)]
 #![allow(incomplete_features)]
 
